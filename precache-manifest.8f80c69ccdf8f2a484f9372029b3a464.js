0fad8c4e79b43153d4d8b7354bdc8069417d6088
Henrique Lopes <webapps@cletile.com>
2019-03-06 17:09:25 -0500

    Vcg: Update live demo

---
+++ precache-manifest.8f80c69ccdf8f2a484f9372029b3a464.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "90ff1f20259471ec70cd18f4747c1946",
+    "url": "/fart-board/index.html"
+  },
+  {
+    "revision": "b8a9774b078d831ffc627ec42b14eca9",
+    "url": "/fart-board/sounds/fart-12.mp3"
+  },
+  {
+    "revision": "4f24c1b94ea4207ba47c1e69ac225096",
+    "url": "/fart-board/favicon-32x32.png"
+  },
+  {
+    "revision": "ad4d117792b7080c1485",
+    "url": "/fart-board/js/chunk-vendors.a94e7df9.js"
+  },
+  {
+    "revision": "3cbd5ae4c269df0f202d0bc501678cd5",
+    "url": "/fart-board/sounds/fart-09.mp3"
+  },
+  {
+    "revision": "27f0962ceb2da38565654e5f08c1a0c5",
+    "url": "/fart-board/sounds/fart-06.mp3"
+  },
+  {
+    "revision": "7e9bca451571106096a8166232785750",
+    "url": "/fart-board/sounds/fart-11.mp3"
+  },
+  {
+    "revision": "07d4915131a68557d900c528f0980a6f",
+    "url": "/fart-board/sounds/fart-10.mp3"
+  },
+  {
+    "revision": "3385ff5925dce174ae44b0afbdea71b9",
+    "url": "/fart-board/sounds/fart-08.mp3"
+  },
+  {
+    "revision": "ed599c44fc78ea634af422b0cf6a7593",
+    "url": "/fart-board/sounds/fart-04.mp3"
+  },
+  {
+    "revision": "d169eb47d480aebc52aa0c193e410566",
+    "url": "/fart-board/sounds/fart-07.mp3"
+  },
+  {
+    "revision": "6d8dbb5bea4e68a8686928ccce1e5bfe",
+    "url": "/fart-board/sounds/fart-02.mp3"
+  },
+  {
+    "revision": "7951e0313d23683d915968170705ef7c",
+    "url": "/fart-board/sounds/fart-03.mp3"
+  },
+  {
+    "revision": "89a868bef450a3c8cfae2ae8ce480eb2",
+    "url": "/fart-board/sounds/fart-05.mp3"
+  },
+  {
+    "revision": "b22c6118dd74b89fb96618ee7fa2fe33",
+    "url": "/fart-board/sounds/fart-01.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fart-board/robots.txt"
+  },
+  {
+    "revision": "cda8e0e07484bf3411d860e9f59d776c",
+    "url": "/fart-board/img/fart-07.cda8e0e0.svg"
+  },
+  {
+    "revision": "931b8bd49baf566103f2",
+    "url": "/fart-board/js/app.c3a3782a.js"
+  },
+  {
+    "revision": "4bd1c1b8e93d4a431dd1400a730e89ad",
+    "url": "/fart-board/img/fart-06.4bd1c1b8.svg"
+  },
+  {
+    "revision": "28e116fe6b1212c91aa5a850e7f4a4cf",
+    "url": "/fart-board/favicon-16x16.png"
+  },
+  {
+    "revision": "21fe49201d8e757715ce564d27414d2c",
+    "url": "/fart-board/img/fart-10.21fe4920.svg"
+  },
+  {
+    "revision": "575f6a1ecc9ab1c341231eeb9eb31c5f",
+    "url": "/fart-board/img/fart-09.575f6a1e.svg"
+  },
+  {
+    "revision": "b6fa807a6be163513b8678d0d8db8375",
+    "url": "/fart-board/img/fart-08.b6fa807a.svg"
+  },
+  {
+    "revision": "8ea35987f04ad875d18f96fdf0316f62",
+    "url": "/fart-board/img/fart-11.8ea35987.svg"
+  },
+  {
+    "revision": "6aed80d54d19a8403f1ca9ab3d5ffc68",
+    "url": "/fart-board/img/poison.6aed80d5.svg"
+  },
+  {
+    "revision": "17900c4ac5c667ddd7591d2dc2cdc1a3",
+    "url": "/fart-board/img/fart-12.17900c4a.svg"
+  },
+  {
+    "revision": "af94b3897076fa422802e01ffd175084",
+    "url": "/fart-board/img/fart-03.af94b389.svg"
+  },
+  {
+    "revision": "623bf59a2e872370e1a8f1b806619b73",
+    "url": "/fart-board/img/fart-05.623bf59a.svg"
+  },
+  {
+    "revision": "b71dacaf99ecb4ecd66bebee601f75ef",
+    "url": "/fart-board/img/fart-02.b71dacaf.svg"
+  },
+  {
+    "revision": "3ee6dad32f26092f8b8be7e3714c268f",
+    "url": "/fart-board/img/fart-01.3ee6dad3.svg"
+  },
+  {
+    "revision": "6bcb385cfea453dea1c8af410ea355d5",
+    "url": "/fart-board/img/fart-04.6bcb385c.svg"
+  },
+  {
+    "revision": "931b8bd49baf566103f2",
+    "url": "/fart-board/css/app.681542ca.css"
+  }
+];
