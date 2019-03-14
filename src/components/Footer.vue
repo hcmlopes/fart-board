d86671a26627857c4cb4aa4c24b784226bbce8e7
Henrique Lopes <webapps@cletile.com>
2019-03-14 12:57:28 -0400

    fix footer credits

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,6 @@
 <template>
   <footer>
     <p>
-      Logo icon made by
-      <a href="https://www.flaticon.com/authors/surang" title="surang">surang</a> from
-      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
-      <a
-        href="http://creativecommons.org/licenses/by/3.0/"
-        title="Creative Commons BY 3.0"
-        target="_blank"
-      >CC 3.0 BY</a>
-    </p>
-    <p>
       Fart icons made by
       <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from
       <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by
